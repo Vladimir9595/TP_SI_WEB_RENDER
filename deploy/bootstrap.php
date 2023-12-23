ab6c4a02b160e9f6aba05ea683eabb7453cdb5d6
Vladimir9595 <vladisac@hotmail.it>
2023-12-23 17:04:19 +0100

    feat : add BDD postgreSQL

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -1,22 +1,20 @@
 <?php
-	use Doctrine\ORM\Tools\Setup;
-	use Doctrine\ORM\EntityManager;
-	date_default_timezone_set('America/Lima');
-	require_once "vendor/autoload.php";
-	$isDevMode = true;
-	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
-	$conn = array(
-	'host' => 'dpg-clvf4ida73kc73bp3kfg-a.oregon-postgres.render.com',
 
-	'driver' => 'pdo_pgsql',
-	'user' => 'cnam_db_akkt_user',
-	'password' => 'I8TW2jVgiSyQDguxH8De6kWjgJ6VjoR4',
-	'dbname' => 'cnam_db_akkt',
-	'port' => '5432'
-	);
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 
-
-	$entityManager = EntityManager::create($conn, $config);
+date_default_timezone_set('America/Lima');
+require_once "vendor/autoload.php";
+$isDevMode = true;
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+$conn = array(
+    'host' => 'dpg-cm23hmvqd2ns73d8e1cg-a.frankfurt-postgres.render.com',
+    'driver' => 'pdo_pgsql',
+    'user' => 'si_web_sql_user',
+    'password' => 'kmGK6TiocRkqoD5SgTn2p2muD2YFJeCE',
+    'dbname' => 'si_web_sql',
+    'port' => '5432'
+);
 
 
-
+$entityManager = EntityManager::create($conn, $config);
