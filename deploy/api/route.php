43df1ad36baeaf03206c63d5213751c9cbee44b9
Vladimir9595 <vladisac@hotmail.it>
2023-12-29 18:23:01 +0100

    feat : add routes for create

--- deploy/api/route.php
+++ deploy/api/route.php
@@ -1,22 +1,23 @@
 <?php
 
-	$app->get('/api/hello/{name}', 'hello');
+$app->get('/api/hello/{name}', 'hello');
 
-	$app->options('/api/catalogue', 'optionsCatalogue' );
+$app->options('/api/catalogue', 'optionsCatalogue');
 
-	// API Nécessitant un Jwt valide
-	$app->get('/api/catalogue/{filtre}', 'getSearchCalatogue' );
+// API Nécessitant un Jwt valide
+$app->get('/api/catalogue/{filtre}', 'getSearchCalatogue');
 
-	// API Nécessitant un Jwt valide
-	$app->get('/api/catalogue', 'getCatalogue');
+// API Nécessitant un Jwt valide
+$app->get('/api/catalogue', 'getCatalogue');
 
-	$app->options('/api/utilisateur', 'optionsUtilisateur');
+$app->options('/api/utilisateur', 'optionsUtilisateur');
 
-	// API Nécessitant un Jwt valide
-	$app->get('/api/utilisateur', 'getUtilisateur');
+$app->post('/api/utilisateur/create', 'createUtilisateur');
 
-	// APi d'authentification générant un JWT
-	$app->post('/api/utilisateur/login', 'postLogin');
-	
-	
+// API Nécessitant un Jwt valide
+$app->get('/api/utilisateur', 'getUtilisateur');
 
+// APi d'authentification générant un JWT
+$app->post('/api/utilisateur/login', 'postLogin');
+
+$app->post('/api/product/create', 'createProduct');
